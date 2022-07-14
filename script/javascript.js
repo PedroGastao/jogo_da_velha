26da87a59dac91d6991c2637d28b343b9a01f835
Pedro Gastão <pedro.gastao.14@gmail.com>
2022-07-14 16:18:59 -0300

    adicionando a mecânica do jogo

--- script/javascript.js
+++ script/javascript.js
@@ -18,8 +18,62 @@ for(let i =0;i < boxes.length; i++){
         }else{
             jogada = o
         }
-        let cloneJogada = jogada.cloneNode(true)
+        if(this.childNodes.length == 0){
+            let cloneJogada = jogada.cloneNode(true)
 
-        this.appendChild(cloneJogada)
+            this.appendChild(cloneJogada)
+    
+            if(player1 == player2){
+                player1++
+            }else{
+                player2++
+            } 
+            checandoVencedor()
+        }
     })
+}
+
+
+function checandoVencedor(){
+    let box1 = document.getElementById("bloco-1")
+    let box2 = document.getElementById("bloco-2")
+    let box3 = document.getElementById("bloco-3")
+    let box4 = document.getElementById("bloco-4")
+    let box5 = document.getElementById("bloco-5")
+    let box6 = document.getElementById("bloco-6")
+    let box7 = document.getElementById("bloco-7")
+    let box8 = document.getElementById("bloco-8")
+    let box9 = document.getElementById("bloco-9")
+
+   /* if(box1.childNodes.length>0 && box2.childNodes.length>0 && box3.childNodes.length>0 ){
+        let child1 = box1.childNodes[0].className
+        let child2 = box2.childNodes[0].className
+        let child3 = box3.childNodes[0].className
+        
+        if(child1 == "x" && child2 == "x" && child3 == "x"){
+            console.log("X venceu")
+        }else if(child1 == "o" && child2 == "o" && child3 == "o"){
+            console.log("o venceu")
+        }
+    } */    
+   
+   
+   
+     function checando(b1,b2,b3){
+        if(b1.childNodes.length>0 && b2.childNodes.length>0 && b3.childNodes.length>0 ){
+            let child1 = b1.childNodes[0].className
+            let child2 = b2.childNodes[0].className
+            let child3 = b3.childNodes[0].className
+            
+            if(child1 == "x" && child2 == "x" && child3 == "x"){
+                return console.log("X venceu")
+            }else if(child1 == "o" && child2 == "o" && child3 == "o"){
+                return console.log("o venceu")
+            }
+        }
+
+    }
+
+    checando(box1,box2,box3) 
+    checando(box4,box5,box6)
 }
